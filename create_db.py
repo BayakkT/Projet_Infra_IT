12a6bbbbcda04202ee8df53b137f83816850818f
BayakkT <tatianaabs25@gmail.com>
2026-01-24 15:05:48 +0100

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -2,20 +2,10 @@ import sqlite3
 
 connection = sqlite3.connect('database.db')
 
-with open('schema.sql') as f:
+with open('schema.sql', encoding='utf-8') as f:
     connection.executescript(f.read())
-
-cur = connection.cursor()
-
-# On insère quelques livres pour commencer
-cur.execute("INSERT INTO livres (titre, auteur, stock) VALUES (?, ?, ?)",
-            ('Harry Potter à l\'école des sorciers', 'J.K. Rowling', 3))
-
-cur.execute("INSERT INTO livres (titre, auteur, stock) VALUES (?, ?, ?)",
-            ('Le Petit Prince', 'Antoine de Saint-Exupéry', 5))
-
-cur.execute("INSERT INTO livres (titre, auteur, stock) VALUES (?, ?, ?)",
-            ('1984', 'George Orwell', 2))
 
 connection.commit()
 connection.close()
+
+print("✅ Base de données tâches créée (database.db)")
